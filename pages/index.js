c889802e680302787674993047eb1d7d69a91c39
Maxime Laforet <me@macx.im>
2020-12-21 08:58:33 +0000

    Fix indentation

--- pages/index.js
+++ pages/index.js
@@ -8,23 +8,21 @@ export default function IndexPage({ allPosts }) {
       <Layout />
       <main className="flex flex-col items-center max-w-4xl">
         <section className="flex-1 p-8 text-2xl leading-normal">
-        <h1 className="mb-4 text-4xl lg:text-5xl">RTFM</h1>
-
-        <ul>
-          {allPosts.map((post) => (
-            <li key={post.slug}>
-              <Link as={`/posts/${post.slug}`} href="/posts/[slug]">
-                <a className="p-2 -mx-2 flex flex-col md:flex-row justify-between focus:ring focus:ring-gray-300 focus:outline-none rounded-md">
-                  <h4 className="hover:underline text-lg md:text-xl">
-                  {post.title}
-                  </h4>
-                  <p className="cursor-default leading-8 text-base text-gray-600 text-left md:text-right md:mb-0 md:ml-16">{post.date}</p>
-                </a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-
+          <h1 className="mb-4 text-4xl lg:text-5xl">RTFM</h1>
+          <ul>
+            {allPosts.map((post) => (
+              <li key={post.slug}>
+                <Link as={`/posts/${post.slug}`} href="/posts/[slug]">
+                  <a className="p-2 -mx-2 flex flex-col md:flex-row justify-between focus:ring focus:ring-gray-300 focus:outline-none rounded-md">
+                    <h4 className="hover:underline text-lg md:text-xl">
+                    {post.title}
+                    </h4>
+                    <p className="cursor-default leading-8 text-base text-gray-600 text-left md:text-right md:mb-0 md:ml-16">{post.date}</p>
+                  </a>
+                </Link>
+              </li>
+            ))}
+          </ul>
         </section>
       </main>
     </>
